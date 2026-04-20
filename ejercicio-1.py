159c920baa8f3748a6476f6a3577fbde35026968
elmelofinooo <hectorabram2005@gmail.com>
2026-04-20 17:23:40 -0500

    feat: se termino de implementar el constructor alternativo

--- ejercicio-1.py
+++ ejercicio-1.py
@@ -1,4 +1,4 @@
-class productosAmazonicos:
+class ProductoAmazonico:
     total_productos =+ 1
 
 #CONSTRUCTOR
@@ -24,12 +24,47 @@ class productosAmazonicos:
         else:
             self.stock -= cantidad
             return "se vendio con exito"
-             
-      
         
-f1= productosAmazonicos("Aguaje","fruta",12,4)
-print(f1.stock)
-f1.vender()
+    def reabastecer(self, cantidad: int):
+        self.stock += cantidad
+    
+    def valor_inventario(self) -> float:
+        return self.precio * self.stock
+    
+    @classmethod
+    def desde_cadena(cls, cadena: str):
+        partes = cadena.replace("|", " ").split()
+        return cls(partes[0], partes[1], float(partes[2]), int(partes[3]))
+    
+    #FORMATOS DE SALIDA
+
+    def __str__(self):
+        return f"{self.nombre} ({self.categoria}) S/ {self.precio:.2f} Stock: {self.stock} unidades"
+    
+    def __repr__(self):
+        return f"ProductoAmazonico('{self.nombre}', '{self.categoria}', {self.precio}, {self.stock})"
+             
+    #TESTEO
+    # --- PROGRAMA DE PRUEBA OBLIGATORIO ---
+if __name__ == "__main__":
+
+    # 1. Crear 5 productos usando constructor normal
+    p1 = ProductoAmazonico("Aguaje", "fruta", 5, 100)
+    p2 = ProductoAmazonico("Camu Camu", "fruta", 8, 50)
+    p3 = ProductoAmazonico("Uña de Gato", "planta medicinal", 15, 20)
+    p4 = ProductoAmazonico("Chonta", "alimento", 10, 15)
+    p5 = ProductoAmazonico("Cerámica", "artesanía", 50, 5)
+
+    print(p1.nombre)
+
+    #CONSTRUCTOR ALTERNATIVO
+    p6 = ProductoAmazonico.desde_cadena("SachaInchi alimento 25.0|40")
+    
+    productos = [p1, p2, p3, p4, p5, p6]
+
+    
+
+        
 
 
 

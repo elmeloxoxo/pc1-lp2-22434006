ef24f6050184e7316aa100ef3bc32e730f1f0574
elmelofinooo <hectorabram2005@gmail.com>
2026-04-21 15:57:06 -0500

    feat: se agrego class curso

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,6 +6,9 @@ class Estudiante:
         self.codigo = codigo
         self.ciclo = ciclo
 
+        #atributo de clase para contar el total de estudiantes
+        Estudiante.total_estudiantes +=1
+
 #Validaciones
         if not (1<= self.ciclo<=10):
             raise ValueError
@@ -24,13 +27,28 @@ class Estudiante:
 
 Est1 = Estudiante("Juan Kiro","22435416",10)
 Est2 = Estudiante("Julio Quiroz","22435417",3)
+Est3 = Estudiante("Brian Alvarado","54356241",6)
 
-print(Est1)
-print(Est2)
 
+print(Estudiante.total_estudiantes)
+
+# print(Est1)
+# print(Est2)
 # print(Est1 == Est2)
 # if Est1 == Est2 :
 #     print ("Ese estudiante ya existe por lo tanto no puede ser registrado.")
 
+class Curso:
+    def __init__(self,nombre:str,codigo:str,creditos:int,ciclo_minimo:int,cupo_maximo:int):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.creditos = creditos
+        self.ciclo_minimo = ciclo_minimo
+        self.cupo_maximo = cupo_maximo
+        self.inscritos = []
 
+    def hay_cupo(self):
+        pass
+
+    
     
